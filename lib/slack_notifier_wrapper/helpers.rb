ef0d707e5f1b2b123044e6da06644f9928ceca00
MrPowers <matthewkevinpowers@gmail.com>
2015-09-09 09:37:09 -0500

    Update a typo in the code

--- lib/slack_notifier_wrapper/helpers.rb
+++ lib/slack_notifier_wrapper/helpers.rb
@@ -1,9 +1,9 @@
 module SlackNotifierWrapper
   module Helpers
 
-    def execute_or_speak(method)
+    def execute_or_speak(method_name)
       begin
-        send(method)
+        send(method_name)
       rescue Exception => e
         attachment_message = "#{self.class}##{method_name} failed to execute"
         SlackNotifierWrapper.speak_attachments({ color: "danger", attachment_message: attachment_message })
